9d199bc43e14935d1805fab9fa796825f2e04347
manuelpringols <manuelpringols@gmail.com>
2025-06-18 12:58:44 +0000

    script inizialize vpn rev:0.1.1

--- setup_vpn/start_vpn_setups.sh
+++ setup_vpn/start_vpn_setups.sh
@@ -4,18 +4,20 @@ TMP_DIR="/tmp/config"
 
 echo "[+] Rimuovo vecchia cartella temporanea..."
 rm -rf "$TMP_DIR"
+rm -rf /tmp/scripts_temp
 
 echo "[+] Clono solo la cartella config del repo..."
 git clone --depth 1 --filter=blob:none --sparse https://github.com/manuelpringols/scripts.git /tmp/scripts_temp
 cd /tmp/scripts_temp || exit 1
+
 git sparse-checkout init --cone
 git sparse-checkout set setup_vpn/config
 
-echo "[+] Copio config in $TMP_DIR"
+echo "[+] Copio tutto il contenuto di config/ in $TMP_DIR"
 mkdir -p "$TMP_DIR"
 cp -r setup_vpn/config/* "$TMP_DIR/"
 
-echo "[+] Rimuovo cartella temporanea git..."
+echo "[+] Rimuovo la cartella temporanea git clonata..."
 rm -rf /tmp/scripts_temp
 
 echo "[+] Entro in $TMP_DIR ed eseguo initialize_script_vpn.sh"
