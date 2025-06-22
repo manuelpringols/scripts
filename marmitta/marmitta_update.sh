f66bd79096e85d045527d1b657e25110784fc39d
manuelpringols <manuelpringols@gmail.com>
2025-06-22 18:12:54 +0000

    marmitta e marmitta_update updated

--- marmitta/marmitta_update.sh
+++ marmitta/marmitta_update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/manuelpringols/scripts/master/marmitta/marmitta.sh -o marmitta.sh && \
-chmod +x marmitta.sh && \
-sudo cp marmitta.sh /usr/local/bin/marmitta
+sudo curl -fsSL https://raw.githubusercontent.com/manuelpringols/scripts/master/marmitta/marmitta.sh -o /usr/local/bin/marmitta && sudo chmod +x /usr/local/bin/marmitta
+
